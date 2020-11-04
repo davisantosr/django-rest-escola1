b43972d099df63f8c5cd2e533e1118c83f5123ca
davisantosr <davi-rocha2@hotmail.com>
2020-11-04 16:57:13 -0300

    added post method on APIView

--- cursos/views.py
+++ cursos/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from. serializers import CursoSerializer, AvaliacaoSerializer
+
 
 class CursoApiView(APIView):
   """ Api de Cursos"""
@@ -12,11 +14,24 @@ class CursoApiView(APIView):
     serializer = CursoSerializer(cursos, many=True)
     return Response(serializer.data)
 
+  def post(self, request):
+    serializer = CursoSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+
 class AvaliacaoAPIView(APIView):
   """Api de Avaliações"""
   def get(self, request):
     avaliacoes = Avaliacao.objects.all()
     serializer = AvaliacaoSerializer(avaliacoes, many=True)
     return Response(serializer.data)
+
+  def post(self, request):
+    serializer = AvaliacaoSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status = status.HTTP_201_CREATED)
 
   
