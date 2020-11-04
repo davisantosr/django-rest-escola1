9563485a22065e54707687f336fb8816f7e97058
davisantosr <davi-rocha2@hotmail.com>
2020-11-04 15:50:47 -0300

    started project|added escola app|config rest|added initial views and urls endpoints

---
+++ cursos/views.py
@@ -0,0 +1,22 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Curso, Avaliacao
+from. serializers import CursoSerializer, AvaliacaoSerializer
+
+class CursoApiView(APIView):
+  """ Api de Cursos"""
+
+  def get(self, request):
+    cursos = Curso.objects.all()
+    serializer = CursoSerializer(cursos, many=True)
+    return Response(serializer.data)
+
+class AvaliacaoAPIView(APIView):
+  """Api de Avaliações"""
+  def get(self, request):
+    avaliacoes = Avaliacao.objects.all()
+    serializer = AvaliacaoSerializer(avaliacoes, many=True)
+    return Response(serializer.data)
+
+  
