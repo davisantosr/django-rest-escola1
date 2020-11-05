99bf67d57727de527713bcf721c18a165b15020d
davisantosr <davi-rocha2@hotmail.com>
2020-11-05 06:04:26 -0300

    Using ListCreateAPIView and ListCreateAPIView|started new endpoint

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import CursoAPIView, AvaliacaoAPIView
-
+from .views import CursoAPIView, CursosAPIView, AvaliacaoAPIView, AvaliacoesAPIView
 urlpatterns = [
-  path('cursos/', CursoAPIView.as_view(), name='cursos'),
-  path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+  path('cursos/', CursosAPIView.as_view(), name='cursos'),
+  path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
+  path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view()),
+#<int:curso_pk> tell to developer which pk we are setting
+  path('cursos/<int:curso_pk>avaliacoes/<int:avaliacao_pk/', AvaliacaoAPIView.as_view(), name='curso_avaliacao')
+
+  path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+  path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
+
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -3,11 +3,21 @@ from rest_framework import generics
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
-class CursoAPIView(generics.ListCreateAPIView):
+class CursosAPIView(generics.ListCreateAPIView):
   queryset = Curso.objects.all()
   serializer_class = CursoSerializer
 
-class AvaliacaoAPIView(generics.ListCreateAPIView):
+
+class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Curso.objects.all()
+  serializer_class = CursoSerializer
+
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
   queryset = Avaliacao.objects.all()
   serializer_class = AvaliacaoSerializer
-  
+
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Avaliacao.objects.all()
+  serializer_class = AvaliacaoSerializer
