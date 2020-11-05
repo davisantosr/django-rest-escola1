3eccf0810a93ed49c4854e383129f1c41bc33451
davisantosr <davi-rocha2@hotmail.com>
2020-11-05 13:36:13 -0300

    added endpoints with parameters|overriding methods from generics(get_object and get_queryset)

--- cursos/urls.py
+++ cursos/urls.py
@@ -4,11 +4,12 @@ from .views import CursoAPIView, CursosAPIView, AvaliacaoAPIView, AvaliacoesAPIV
 urlpatterns = [
   path('cursos/', CursosAPIView.as_view(), name='cursos'),
   path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
-  path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view()),
-#<int:curso_pk> tell to developer which pk we are setting
-  path('cursos/<int:curso_pk>avaliacoes/<int:avaliacao_pk/', AvaliacaoAPIView.as_view(), name='curso_avaliacao')
+  path('cursos/<int:curso_pk>/avaliacoes/', AvaliacoesAPIView.as_view(), name='curso_avaliacoes'),
+  path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk/', AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
+
+#<int:curso_pk> and <int:avaliacao_pk> tell to developer which pk we are setting
 
   path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
-  path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
+  path('avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
 
 ]
